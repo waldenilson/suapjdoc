a8365ee8c7ae15814ab4e9c0e6f7fe4aed7073ad
Waldenilson <waldenilsonnovaes@gmail.com>
2016-11-22 17:18:04 -0300

    criacao da entidade e dao do objeto AuthGroup.

---
+++ src/br/com/nead/suapjdoc/entity/AuthGroup.java
@@ -0,0 +1,44 @@
+package br.com.nead.suapjdoc.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class AuthGroup implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@SequenceGenerator(name="AUTH_GROUP_ID_GENERATOR", sequenceName="AUTH_GROUP_ID_SEQ", allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="AUTH_GROUP_ID_GENERATOR")
+	private int id;
+	
+	@Column
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
