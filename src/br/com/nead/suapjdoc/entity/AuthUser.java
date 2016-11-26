7fc6bfa879c3177570a0d715b113fa45fcdcc56a
Waldenilson Carneiro <waldenilsonnovaes@gmail.com>
2016-11-26 19:16:36 -0300

    adicao dos atributos e anotacoes das entidades auth_user e documento.

--- src/br/com/nead/suapjdoc/entity/AuthUser.java
+++ src/br/com/nead/suapjdoc/entity/AuthUser.java
@@ -1,12 +1,18 @@
 package br.com.nead.suapjdoc.entity;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class AuthUser implements Serializable {
@@ -20,6 +26,32 @@ public class AuthUser implements Serializable {
 	@SequenceGenerator(name="AUTH_USER_ID_GENERATOR", sequenceName="AUTH_USER_ID_SEQ", allocationSize=1)
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="AUTH_USER_ID_GENERATOR")
 	private int id;
+
+	private String username;
+	private String password;
+	
+	@Column(name="first_name")
+	private String firstName;
+
+	@Column(name="last_name")
+	private String lastName;
+	private String email;
+	
+	@Column(name="is_staff")
+	private Boolean isStaff;
+
+	@Column(name="is_active")
+	private Boolean isActive;
+	
+	@Column(name="is_superuser")
+	private Boolean isSuperuser;
+	
+	@Column(name="date_joined")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dateJoined;
+
+	@OneToMany(mappedBy="authUserId")
+	private List<Documento> documentos;
 	
 	public int getId() {
 		return id;
@@ -28,5 +60,86 @@ public class AuthUser implements Serializable {
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Boolean getIsStaff() {
+		return isStaff;
+	}
+
+	public void setIsStaff(Boolean isStaff) {
+		this.isStaff = isStaff;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public Boolean getIsSuperuser() {
+		return isSuperuser;
+	}
+
+	public void setIsSuperuser(Boolean isSuperuser) {
+		this.isSuperuser = isSuperuser;
+	}
+
+	public Date getDateJoined() {
+		return dateJoined;
+	}
+
+	public void setDateJoined(Date dateJoined) {
+		this.dateJoined = dateJoined;
+	}
+
+	public List<Documento> getDocumentos() {
+		return documentos;
+	}
+
+	public void setDocumentos(List<Documento> documentos) {
+		this.documentos = documentos;
+	}
+	
 	
 }
--- src/br/com/nead/suapjdoc/entity/Documento.java
+++ src/br/com/nead/suapjdoc/entity/Documento.java
@@ -1,12 +1,18 @@
 package br.com.nead.suapjdoc.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Documento implements Serializable {
@@ -21,6 +27,35 @@ public class Documento implements Serializable {
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="DOCUMENTO_ID_GENERATOR")
 	private int id;
 	
+	private Integer numero;
+	private Integer ano;
+	private String setor;
+	private String remetente;
+
+	@Column(name="instituicao_destinatario")
+	private String instituicaoDestinatario;
+	private String destinatario;
+	private String signatario;
+	
+	@Column(name="cargo_signatario")
+	private String cargoSignatario;
+	private String assunto;
+	private String mensagem;
+
+	@Column(name="data_documento")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataDocumento;
+	
+	@Column(name="data_cadastro")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCadastro;
+	
+	private String tipo;
+	
+	@ManyToOne
+	@JoinColumn(name="auth_user_id")
+	private AuthUser authUserId;
+	
 	public int getId() {
 		return id;
 	}
@@ -28,5 +63,117 @@ public class Documento implements Serializable {
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	public String getSetor() {
+		return setor;
+	}
+
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+
+	public String getRemetente() {
+		return remetente;
+	}
+
+	public void setRemetente(String remetente) {
+		this.remetente = remetente;
+	}
+
+	public String getInstituicaoDestinatario() {
+		return instituicaoDestinatario;
+	}
+
+	public void setInstituicaoDestinatario(String instituicaoDestinatario) {
+		this.instituicaoDestinatario = instituicaoDestinatario;
+	}
+
+	public String getDestinatario() {
+		return destinatario;
+	}
+
+	public void setDestinatario(String destinatario) {
+		this.destinatario = destinatario;
+	}
+
+	public String getSignatario() {
+		return signatario;
+	}
+
+	public void setSignatario(String signatario) {
+		this.signatario = signatario;
+	}
+
+	public String getCargoSignatario() {
+		return cargoSignatario;
+	}
+
+	public void setCargoSignatario(String cargoSignatario) {
+		this.cargoSignatario = cargoSignatario;
+	}
+
+	public String getAssunto() {
+		return assunto;
+	}
+
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public Date getDataDocumento() {
+		return dataDocumento;
+	}
+
+	public void setDataDocumento(Date dataDocumento) {
+		this.dataDocumento = dataDocumento;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public AuthUser getAuthUserId() {
+		return authUserId;
+	}
+
+	public void setAuthUserId(AuthUser authUserId) {
+		this.authUserId = authUserId;
+	}
+
 }
